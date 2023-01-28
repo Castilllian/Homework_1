1a2b7ee2a2edae3764a49deeb2550af4dafb3c55
Konstantin Senchenkov <senchenkovko@gmail.com>
2023-01-28 17:54:11 +0400

    Solve problem Task_2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Clear();
+Console.Write("Enter number1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Enter number2: ");
+int number2 = int.Parse(Console.ReadLine());
+int max = number1;
+if (number2 > number1 ){
+    Console.WriteLine($"Максимальное из чисел {number1} и {number2} является {number2}");
+}
+else{
+    Console.WriteLine($"Максимальное из чисел {number1} и {number2} является {number1}");
+}
